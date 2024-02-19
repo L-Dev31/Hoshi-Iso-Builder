9ecea07f55a11e2d557868912c40683741a1a81f
Léo/L-Dev <86838693+L-Dev31@users.noreply.github.com>
2024-02-19 13:03:07 -0400

    yes

--- USBLoaderGXPlus/source/Riivolution/riivolutionmenu.cpp
+++ USBLoaderGXPlus/source/Riivolution/riivolutionmenu.cpp
@@ -9,97 +9,197 @@
 #include "FileOperations/fileops.h"
 #include "menu.h"
 #include "sys.h"
+#include "gct.h"
 
 /****************************************************************************
- * RiivolutionMenu 
- * L-DEV 2024
- * The source code as well as any code uses are the property of USBLoaderGX and Riivolution
- * Use this functionality at your own risk; I do not endorse any issues, and it is your responsibility to use this tool in a legal manner.
+ * RiivolutionMenu
+ * based off cheatmenu (to change !!!)
  ***************************************************************************/
-int RiivolutionMenu(const char *gameID)
+int RiivolutionMenu(const char * gameID)
 {
-    int choice = 0;
-    bool exit = false;
-    int ret = 1;
+	int choice = 0;
+	bool exit = false;
+	int ret = 1;
 
-    GuiImageData btnOutline(Resources::GetFile("button_dialogue_box.png"), Resources::GetFileSize("button_dialogue_box.png"));
-    GuiImageData settingsbg(Resources::GetFile("settings_background.png"), Resources::GetFileSize("settings_background.png"));
-    GuiImage settingsbackground(&settingsbg);
+	GuiImageData btnOutline(Resources::GetFile("button_dialogue_box.png"), Resources::GetFileSize("button_dialogue_box.png"));
+	GuiImageData settingsbg(Resources::GetFile("settings_background.png"), Resources::GetFileSize("settings_background.png"));
+	GuiImage settingsbackground(&settingsbg);
 
-    GuiTrigger trigA;
-    trigA.SetSimpleTrigger(-1, WPAD_BUTTON_A | WPAD_CLASSIC_BUTTON_A, PAD_BUTTON_A);
-    GuiTrigger trigB;
-    trigB.SetButtonOnlyTrigger(-1, WPAD_BUTTON_B | WPAD_CLASSIC_BUTTON_B, PAD_BUTTON_B);
+	GuiTrigger trigA;
+	trigA.SetSimpleTrigger(-1, WPAD_BUTTON_A | WPAD_CLASSIC_BUTTON_A, PAD_BUTTON_A);
+	GuiTrigger trigB;
+	trigB.SetButtonOnlyTrigger(-1, WPAD_BUTTON_B | WPAD_CLASSIC_BUTTON_B, PAD_BUTTON_B);
 
-    GuiText backBtnTxt(tr("Back"), 22, thColor("r=0 g=0 b=0 a=255 - prompt windows button text color"));
-    backBtnTxt.SetMaxWidth(btnOutline.GetWidth() - 30);
-    GuiImage backBtnImg(&btnOutline);
-    GuiButton backBtn(&backBtnImg, &backBtnImg, 2, 3, -195, 400, &trigA, NULL, btnSoundClick2, 1);
-    backBtn.SetLabel(&backBtnTxt);
-    backBtn.SetTrigger(&trigB);
+	GuiText backBtnTxt(tr( "Back" ), 22, thColor("r=0 g=0 b=0 a=255 - prompt windows button text color"));
+	backBtnTxt.SetMaxWidth(btnOutline.GetWidth() - 30);
+	GuiImage backBtnImg(&btnOutline);
+	GuiButton backBtn(&backBtnImg, &backBtnImg, 2, 3, -195, 400, &trigA, NULL, btnSoundClick2, 1);
+	backBtn.SetLabel(&backBtnTxt);
+	backBtn.SetTrigger(&trigB);
 
-    char riivolutionFolderPath[120];
-    snprintf(riivolutionFolderPath, sizeof(riivolutionFolderPath), "%s%s/", Settings.XmlRiivolutionpath, gameID);
+	char txtfilename[120];
+	snprintf(txtfilename, sizeof(txtfilename), "%s%s.txt", Settings.TxtCheatcodespath, gameID);
 
-    if (!DirectoryExist(riivolutionFolderPath))
-    {
-        choice = WindowPrompt(tr("Error"), tr("Riivolution folder not found. Create it?"), tr("Yes"), tr("Cancel"));
-        if (choice)
-        {
-            CreateSubfolder(riivolutionFolderPath);
-        }
-        else
-        {
-            return choice;
-        }
-    }
+	GCTCheats gctCheats;
+	int check = gctCheats.openTxtfile(txtfilename);
 
-    OptionList riivolutionFiles;
-    GuiOptionBrowser riivolutionBrowser(400, 280, &riivolutionFiles, "bg_options_settings.png");
-    riivolutionBrowser.SetPosition(0, 90);
-    riivolutionBrowser.SetAlignment(ALIGN_CENTER, ALIGN_TOP);
-    riivolutionBrowser.SetClickable(true);
+	int download = 0;
+	int blankchoice = 0;
 
-    GuiText titleTxt(gameID, 28, (GXColor){0, 0, 0, 255});
-    titleTxt.SetAlignment(ALIGN_CENTER, ALIGN_TOP);
-    titleTxt.SetMaxWidth(350, SCROLL_HORIZONTAL);
-    titleTxt.SetPosition(12, 40);
+	switch (check)
+	{
+		case -1:
+			blankchoice = WindowPrompt(tr( "Error" ), tr( "Cheatfile is blank" ), tr( "Delete" ), tr( "OK" ));
+			if(blankchoice)
+			{
+				char gctPath[200];
+				snprintf(gctPath, sizeof(gctPath), "%s%.6s.TXT", Settings.TxtCheatcodespath, gameID);
+				RemoveFile(gctPath);
+			}
+			break;
+		case 0:
+			download = WindowPrompt(tr( "Error" ), tr( "No Cheatfile found" ), tr( "Download Now" ), tr( "Cancel" ));
+			if (download == 1)
+			{
+				download = CodeDownload(gameID);
+				if (download < 0 || gctCheats.openTxtfile(txtfilename) != 1)
+					break;
+			}
+			else
+				break;
+		case 1:
+			int cntcheats = gctCheats.getCnt();
+			OptionList cheatslst;
+			GuiOptionBrowser chtBrowser(400, 280, &cheatslst, "bg_options_settings.png");
+			chtBrowser.SetPosition(0, 90);
+			chtBrowser.SetAlignment(ALIGN_CENTER, ALIGN_TOP);
+			chtBrowser.SetClickable(true);
 
-    ListDirectory(riivolutionFolderPath, &riivolutionFiles);
+			GuiText titleTxt(gctCheats.getGameName().c_str(), 28, ( GXColor ) {0, 0, 0, 255});
+			titleTxt.SetAlignment(ALIGN_CENTER, ALIGN_TOP);
+			titleTxt.SetMaxWidth(350, SCROLL_HORIZONTAL);
+			titleTxt.SetPosition(12, 40);
 
-    HaltGui();
-    GuiWindow w(screenwidth, screenheight);
-    w.Append(&settingsbackground);
-    w.Append(&titleTxt);
-    w.Append(&backBtn);
-    w.Append(&riivolutionBrowser);
-    mainWindow->SetState(STATE_DISABLED);
-    mainWindow->Append(&w);
-    ResumeGui();
+			char gctPath[200];
+			snprintf(gctPath, sizeof(gctPath), "%s%.6s.gct", Settings.Cheatcodespath, gameID);
+			u8 *gctBuf = NULL;
+			u32 gctSize = 0;
+			LoadFileToMem(gctPath, &gctBuf, &gctSize);
 
-    while (!exit)
-    {
-        usleep(100000);
+			for (int i = 0; i < cntcheats; i++)
+			{
+				cheatslst.SetValue(i, "%s", gctCheats.getCheatName(i).c_str());
+				// search after header and before footer
+				if(gctBuf && gctCheats.IsCheatIncluded(i, gctBuf, gctSize))
+					cheatslst.SetName(i, tr("ON"));
+				else
+					cheatslst.SetName(i, tr("OFF"));
+			}
 
-        ret = riivolutionBrowser.GetClickedOption();
-        if (ret >= 0)
-        {
-            const char *riivolutionFile = riivolutionFiles.GetValue(ret);
-            // Riivolution logic
-        }
+			HaltGui();
+			GuiWindow w(screenwidth, screenheight);
+			w.Append(&settingsbackground);
+			w.Append(&titleTxt);
+			w.Append(&backBtn);
+			w.Append(&updateBtn);
+			w.Append(&createBtn);
+			w.Append(&chtBrowser);
+			mainWindow->SetState(STATE_DISABLED);
+			mainWindow->Append(&w);
+			ResumeGui();
 
-        if (backBtn.GetState() == STATE_CLICKED)
-        {
-            backBtn.ResetState();
-            exit = true;
-            break;
-        }
-    }
+			while (!exit)
+			{
+				usleep(100000);
 
-    HaltGui();
-    mainWindow->SetState(STATE_DEFAULT);
-    mainWindow->Remove(&w);
-    ResumeGui();
+				ret = chtBrowser.GetClickedOption();
+				if (ret >= 0)
+				{
+					const char *strCheck = cheatslst.GetName(ret);
+					if (strCheck && strcmp(strCheck, tr("ON")) == 0)
+					{
+						cheatslst.SetName(ret, "%s", tr("OFF"));
+					}
+					else if (strCheck && strcmp(strCheck, tr("OFF")) == 0)
+					{
+						cheatslst.SetName(ret, "%s", tr("ON"));
+					}
+				}
 
-    return choice;
+				if (createBtn.GetState() == STATE_CLICKED)
+				{
+					if (cntcheats > 0)
+					{
+						std::vector<int> vActiveCheats;
+						for (int i = 0; i < cntcheats; i++)
+						{
+							const char *strCheck = cheatslst.GetName(i);
+							if (strCheck && strcmp(strCheck, tr("ON")) == 0)
+								vActiveCheats.push_back(i);
+						}
+						if (vActiveCheats.size() == 0)
+						{
+							if(WindowPrompt(tr( "Error" ), tr( "No cheats were selected! Should the GCT file be deleted?" ), tr("Yes"), tr("Cancel")))
+							{
+								RemoveFile(gctPath);
+								w.Remove(&chtBrowser);
+								for (int i = 0; i < gctCheats.getCnt(); i++)
+									cheatslst.SetName(i, tr("OFF"));
+								w.Append(&chtBrowser);
+							}
+						}
+						else
+						{
+							CreateSubfolder(Settings.Cheatcodespath);
+							gctCheats.createGCT(vActiveCheats, gctPath);
+							WindowPrompt(tr( "GCT File created" ), NULL, tr( "OK" ));
+						}
+					}
+					else
+						WindowPrompt(tr( "Error" ), tr( "Could not create GCT file" ), tr( "OK" ));
+
+					mainWindow->SetState(STATE_DISABLED);
+					w.SetState(STATE_DEFAULT);
+					createBtn.ResetState();
+				}
+
+				if (backBtn.GetState() == STATE_CLICKED)
+				{
+					backBtn.ResetState();
+					exit = true;
+					break;
+				}
+
+				if(updateBtn.GetState() == STATE_CLICKED)
+				{
+					download = CodeDownload(gameID);
+					if (download >= 0 && gctCheats.openTxtfile(txtfilename) == 1)
+					{
+						w.Remove(&chtBrowser);
+						cheatslst.ClearList();
+						cntcheats = gctCheats.getCnt();
+						for (int i = 0; i < cntcheats; i++)
+						{
+							cheatslst.SetValue(i, "%s", gctCheats.getCheatName(i).c_str());
+							// search after header and before footer
+							if(gctBuf && gctCheats.IsCheatIncluded(i, gctBuf, gctSize))
+								cheatslst.SetName(i, tr("ON"));
+							else
+								cheatslst.SetName(i, tr("OFF"));
+						}
+						w.Append(&chtBrowser);
+					}
+					updateBtn.ResetState();
+				}
+			}
+			if(gctBuf)
+				free(gctBuf);
+			HaltGui();
+			mainWindow->SetState(STATE_DEFAULT);
+			mainWindow->Remove(&w);
+			ResumeGui();
+			break;
+	}
+
+	return choice;
 }
