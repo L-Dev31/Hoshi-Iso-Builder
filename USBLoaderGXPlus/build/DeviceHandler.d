b6cbab4da75ec2e512224c9cd8bb892a2e12287f
Léo/L-Dev <86838693+L-Dev31@users.noreply.github.com>
2024-02-20 16:09:46 -0400

    upd

---
+++ USBLoaderGXPlus/build/DeviceHandler.d
@@ -0,0 +1,152 @@
+DeviceHandler.o: \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/Controls/DeviceHandler.cpp \
+ C:/devkitPro/libogc/include/ogc/mutex.h \
+ C:/devkitPro/libogc/include/gctypes.h \
+ C:/devkitPro/libogc/include/ogc/system.h \
+ C:/devkitPro/libogc/include/gcbool.h \
+ C:/devkitPro/libogc/include/gcutil.h \
+ C:/devkitPro/libogc/include/ogc/lwp_queue.h \
+ C:/devkitPro/libogc/include/ogc/gx_struct.h \
+ C:/devkitPro/libogc/include/sdcard/wiisd_io.h \
+ C:/devkitPro/libogc/include/ogc/disc_io.h \
+ C:/devkitPro/libogc/include/sdcard/gcsd.h \
+ C:/devkitPro/libogc/include/gccore.h \
+ C:/devkitPro/libogc/include/ogc/dsp.h \
+ C:/devkitPro/libogc/include/ogc/aram.h \
+ C:/devkitPro/libogc/include/ogc/arqueue.h \
+ C:/devkitPro/libogc/include/ogc/aram.h \
+ C:/devkitPro/libogc/include/ogc/arqmgr.h \
+ C:/devkitPro/libogc/include/ogc/audio.h \
+ C:/devkitPro/libogc/include/ogc/cache.h \
+ C:/devkitPro/libogc/include/ogc/card.h \
+ C:/devkitPro/libogc/include/ogc/cast.h \
+ C:/devkitPro/libogc/include/ogc/color.h \
+ C:/devkitPro/libogc/include/ogc/consol.h \
+ C:/devkitPro/libogc/include/ogc/dvd.h \
+ C:/devkitPro/libogc/include/ogc/exi.h \
+ C:/devkitPro/libogc/include/ogc/gu.h \
+ C:/devkitPro/libogc/include/ogc/gx.h \
+ C:/devkitPro/libogc/include/ogc/lwp.h \
+ C:/devkitPro/libogc/include/ogc/gu.h \
+ C:/devkitPro/libogc/include/ogc/si.h \
+ C:/devkitPro/libogc/include/ogc/gx_struct.h \
+ C:/devkitPro/libogc/include/ogc/irq.h \
+ C:/devkitPro/libogc/include/ogc/context.h \
+ C:/devkitPro/libogc/include/ogc/lwp.h \
+ C:/devkitPro/libogc/include/ogc/mutex.h \
+ C:/devkitPro/libogc/include/ogc/message.h \
+ C:/devkitPro/libogc/include/ogc/semaphore.h \
+ C:/devkitPro/libogc/include/ogc/pad.h \
+ C:/devkitPro/libogc/include/ogc/tpl.h \
+ C:/devkitPro/libogc/include/ogc/gx.h \
+ C:/devkitPro/libogc/include/ogc/system.h \
+ C:/devkitPro/libogc/include/ogc/video.h \
+ C:/devkitPro/libogc/include/ogc/video_types.h \
+ C:/devkitPro/libogc/include/ogc/usbgecko.h \
+ C:/devkitPro/libogc/include/ogc/video_types.h \
+ C:/devkitPro/libogc/include/ogc/texconv.h \
+ C:/devkitPro/libogc/include/ogc/ipc.h \
+ C:/devkitPro/libogc/include/ogc/sha.h \
+ C:/devkitPro/libogc/include/ogc/aes.h \
+ C:/devkitPro/libogc/include/ogc/es.h \
+ C:/devkitPro/libogc/include/ogc/stm.h \
+ C:/devkitPro/libogc/include/ogc/ios.h \
+ C:/devkitPro/libogc/include/ogc/usb.h \
+ C:/devkitPro/libogc/include/ogc/isfs.h \
+ C:/devkitPro/libogc/include/ogc/conf.h \
+ C:/devkitPro/libogc/include/ogc/usbstorage.h \
+ C:/devkitPro/libogc/include/ogc/wiilaunch.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/Controls/DeviceHandler.hpp \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/usbloader/sdhc.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/Controls/PartitionHandle.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/usbloader/usbstorage2.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/settings/CSettings.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/settings/SettingsEnums.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/libs/libwbfs/wiidisc.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/libs/libwbfs/libwbfs_os.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/memory/mem2.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GameCube/DML_Config.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GameCube/DEVO_Config.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GameCube/NIN_Config.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/usbloader/wbfs.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/libs/libwbfs/libwbfs.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/libs/libwbfs/wiidisc.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/usbloader/disc.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/system/IosLoader.h \
+ C:/devkitPro/libogc/include/ogc/machine/processor.h \
+ C:/devkitPro/libogc/include/ogc/machine/asm.h
+C:/devkitPro/libogc/include/ogc/mutex.h:
+C:/devkitPro/libogc/include/gctypes.h:
+C:/devkitPro/libogc/include/ogc/system.h:
+C:/devkitPro/libogc/include/gcbool.h:
+C:/devkitPro/libogc/include/gcutil.h:
+C:/devkitPro/libogc/include/ogc/lwp_queue.h:
+C:/devkitPro/libogc/include/ogc/gx_struct.h:
+C:/devkitPro/libogc/include/sdcard/wiisd_io.h:
+C:/devkitPro/libogc/include/ogc/disc_io.h:
+C:/devkitPro/libogc/include/sdcard/gcsd.h:
+C:/devkitPro/libogc/include/gccore.h:
+C:/devkitPro/libogc/include/ogc/dsp.h:
+C:/devkitPro/libogc/include/ogc/aram.h:
+C:/devkitPro/libogc/include/ogc/arqueue.h:
+C:/devkitPro/libogc/include/ogc/aram.h:
+C:/devkitPro/libogc/include/ogc/arqmgr.h:
+C:/devkitPro/libogc/include/ogc/audio.h:
+C:/devkitPro/libogc/include/ogc/cache.h:
+C:/devkitPro/libogc/include/ogc/card.h:
+C:/devkitPro/libogc/include/ogc/cast.h:
+C:/devkitPro/libogc/include/ogc/color.h:
+C:/devkitPro/libogc/include/ogc/consol.h:
+C:/devkitPro/libogc/include/ogc/dvd.h:
+C:/devkitPro/libogc/include/ogc/exi.h:
+C:/devkitPro/libogc/include/ogc/gu.h:
+C:/devkitPro/libogc/include/ogc/gx.h:
+C:/devkitPro/libogc/include/ogc/lwp.h:
+C:/devkitPro/libogc/include/ogc/gu.h:
+C:/devkitPro/libogc/include/ogc/si.h:
+C:/devkitPro/libogc/include/ogc/gx_struct.h:
+C:/devkitPro/libogc/include/ogc/irq.h:
+C:/devkitPro/libogc/include/ogc/context.h:
+C:/devkitPro/libogc/include/ogc/lwp.h:
+C:/devkitPro/libogc/include/ogc/mutex.h:
+C:/devkitPro/libogc/include/ogc/message.h:
+C:/devkitPro/libogc/include/ogc/semaphore.h:
+C:/devkitPro/libogc/include/ogc/pad.h:
+C:/devkitPro/libogc/include/ogc/tpl.h:
+C:/devkitPro/libogc/include/ogc/gx.h:
+C:/devkitPro/libogc/include/ogc/system.h:
+C:/devkitPro/libogc/include/ogc/video.h:
+C:/devkitPro/libogc/include/ogc/video_types.h:
+C:/devkitPro/libogc/include/ogc/usbgecko.h:
+C:/devkitPro/libogc/include/ogc/video_types.h:
+C:/devkitPro/libogc/include/ogc/texconv.h:
+C:/devkitPro/libogc/include/ogc/ipc.h:
+C:/devkitPro/libogc/include/ogc/sha.h:
+C:/devkitPro/libogc/include/ogc/aes.h:
+C:/devkitPro/libogc/include/ogc/es.h:
+C:/devkitPro/libogc/include/ogc/stm.h:
+C:/devkitPro/libogc/include/ogc/ios.h:
+C:/devkitPro/libogc/include/ogc/usb.h:
+C:/devkitPro/libogc/include/ogc/isfs.h:
+C:/devkitPro/libogc/include/ogc/conf.h:
+C:/devkitPro/libogc/include/ogc/usbstorage.h:
+C:/devkitPro/libogc/include/ogc/wiilaunch.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/Controls/DeviceHandler.hpp:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/usbloader/sdhc.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/Controls/PartitionHandle.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/usbloader/usbstorage2.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/settings/CSettings.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/settings/SettingsEnums.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/libs/libwbfs/wiidisc.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/libs/libwbfs/libwbfs_os.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/memory/mem2.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GameCube/DML_Config.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GameCube/DEVO_Config.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GameCube/NIN_Config.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/usbloader/wbfs.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/libs/libwbfs/libwbfs.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/libs/libwbfs/wiidisc.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/usbloader/disc.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/system/IosLoader.h:
+C:/devkitPro/libogc/include/ogc/machine/processor.h:
+C:/devkitPro/libogc/include/ogc/machine/asm.h:
---
+++ USBLoaderGXPlus/build/gui_trigger.d
@@ -0,0 +1,182 @@
+gui_trigger.o: \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GUI/gui_trigger.cpp \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GUI/gui.h \
+ C:/devkitPro/libogc/include/gccore.h \
+ C:/devkitPro/libogc/include/ogc/dsp.h \
+ C:/devkitPro/libogc/include/gctypes.h \
+ C:/devkitPro/libogc/include/ogc/aram.h \
+ C:/devkitPro/libogc/include/gcbool.h \
+ C:/devkitPro/libogc/include/ogc/arqueue.h \
+ C:/devkitPro/libogc/include/ogc/lwp_queue.h \
+ C:/devkitPro/libogc/include/ogc/aram.h \
+ C:/devkitPro/libogc/include/ogc/arqmgr.h \
+ C:/devkitPro/libogc/include/ogc/audio.h \
+ C:/devkitPro/libogc/include/ogc/cache.h \
+ C:/devkitPro/libogc/include/ogc/card.h \
+ C:/devkitPro/libogc/include/ogc/cast.h \
+ C:/devkitPro/libogc/include/ogc/color.h \
+ C:/devkitPro/libogc/include/ogc/consol.h \
+ C:/devkitPro/libogc/include/ogc/gx_struct.h \
+ C:/devkitPro/libogc/include/ogc/dvd.h \
+ C:/devkitPro/libogc/include/ogc/disc_io.h \
+ C:/devkitPro/libogc/include/ogc/exi.h \
+ C:/devkitPro/libogc/include/ogc/gu.h \
+ C:/devkitPro/libogc/include/ogc/gx.h \
+ C:/devkitPro/libogc/include/ogc/lwp.h \
+ C:/devkitPro/libogc/include/ogc/gu.h \
+ C:/devkitPro/libogc/include/ogc/si.h \
+ C:/devkitPro/libogc/include/ogc/gx_struct.h \
+ C:/devkitPro/libogc/include/ogc/irq.h \
+ C:/devkitPro/libogc/include/ogc/context.h \
+ C:/devkitPro/libogc/include/ogc/lwp.h \
+ C:/devkitPro/libogc/include/ogc/mutex.h \
+ C:/devkitPro/libogc/include/ogc/message.h \
+ C:/devkitPro/libogc/include/ogc/semaphore.h \
+ C:/devkitPro/libogc/include/ogc/pad.h \
+ C:/devkitPro/libogc/include/ogc/tpl.h \
+ C:/devkitPro/libogc/include/ogc/gx.h \
+ C:/devkitPro/libogc/include/ogc/system.h \
+ C:/devkitPro/libogc/include/gcutil.h \
+ C:/devkitPro/libogc/include/ogc/video.h \
+ C:/devkitPro/libogc/include/ogc/video_types.h \
+ C:/devkitPro/libogc/include/ogc/usbgecko.h \
+ C:/devkitPro/libogc/include/ogc/video_types.h \
+ C:/devkitPro/libogc/include/ogc/texconv.h \
+ C:/devkitPro/libogc/include/ogc/ipc.h \
+ C:/devkitPro/libogc/include/ogc/sha.h \
+ C:/devkitPro/libogc/include/ogc/aes.h \
+ C:/devkitPro/libogc/include/ogc/es.h \
+ C:/devkitPro/libogc/include/ogc/stm.h \
+ C:/devkitPro/libogc/include/ogc/ios.h \
+ C:/devkitPro/libogc/include/ogc/usb.h \
+ C:/devkitPro/libogc/include/ogc/isfs.h \
+ C:/devkitPro/libogc/include/ogc/conf.h \
+ C:/devkitPro/libogc/include/ogc/usbstorage.h \
+ C:/devkitPro/libogc/include/ogc/mutex.h \
+ C:/devkitPro/libogc/include/ogc/system.h \
+ C:/devkitPro/libogc/include/ogc/wiilaunch.h \
+ C:/devkitPro/libogc/include/asndlib.h \
+ C:/devkitPro/libogc/include/wiiuse/wpad.h \
+ C:/devkitPro/libogc/include/wiiuse/wiiuse.h \
+ C:/devkitPro/libogc/include/bte/bte.h \
+ C:/devkitPro/libogc/include/bte/bd_addr.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GUI/gui_imagedata.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/gd.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/gd_io.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/gdfx.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/gd.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/FreeTypeGX.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/ft2build.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/config/ftheader.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/freetype.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/config/ftconfig.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/config/ftoption.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/config/ftstdlib.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/fterrors.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/ftmoderr.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/fterrdef.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/fttypes.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/ftsystem.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/ftimage.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/ftbitmap.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/video.h \
+ C:/devkitPro/libogc/include/ogcsys.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/input.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GUI/OptionList.hpp \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/SoundOperations/gui_sound.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/SoundOperations/gui_bgm.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/SoundOperations/gui_sound.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/utils/timer.h \
+ C:/devkitPro/libogc/include/ogc/lwp_watchdog.h \
+ C:/devkitPro/libogc/include/ogc/lwp_queue.h \
+ E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GUI/sigslot.h
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GUI/gui.h:
+C:/devkitPro/libogc/include/gccore.h:
+C:/devkitPro/libogc/include/ogc/dsp.h:
+C:/devkitPro/libogc/include/gctypes.h:
+C:/devkitPro/libogc/include/ogc/aram.h:
+C:/devkitPro/libogc/include/gcbool.h:
+C:/devkitPro/libogc/include/ogc/arqueue.h:
+C:/devkitPro/libogc/include/ogc/lwp_queue.h:
+C:/devkitPro/libogc/include/ogc/aram.h:
+C:/devkitPro/libogc/include/ogc/arqmgr.h:
+C:/devkitPro/libogc/include/ogc/audio.h:
+C:/devkitPro/libogc/include/ogc/cache.h:
+C:/devkitPro/libogc/include/ogc/card.h:
+C:/devkitPro/libogc/include/ogc/cast.h:
+C:/devkitPro/libogc/include/ogc/color.h:
+C:/devkitPro/libogc/include/ogc/consol.h:
+C:/devkitPro/libogc/include/ogc/gx_struct.h:
+C:/devkitPro/libogc/include/ogc/dvd.h:
+C:/devkitPro/libogc/include/ogc/disc_io.h:
+C:/devkitPro/libogc/include/ogc/exi.h:
+C:/devkitPro/libogc/include/ogc/gu.h:
+C:/devkitPro/libogc/include/ogc/gx.h:
+C:/devkitPro/libogc/include/ogc/lwp.h:
+C:/devkitPro/libogc/include/ogc/gu.h:
+C:/devkitPro/libogc/include/ogc/si.h:
+C:/devkitPro/libogc/include/ogc/gx_struct.h:
+C:/devkitPro/libogc/include/ogc/irq.h:
+C:/devkitPro/libogc/include/ogc/context.h:
+C:/devkitPro/libogc/include/ogc/lwp.h:
+C:/devkitPro/libogc/include/ogc/mutex.h:
+C:/devkitPro/libogc/include/ogc/message.h:
+C:/devkitPro/libogc/include/ogc/semaphore.h:
+C:/devkitPro/libogc/include/ogc/pad.h:
+C:/devkitPro/libogc/include/ogc/tpl.h:
+C:/devkitPro/libogc/include/ogc/gx.h:
+C:/devkitPro/libogc/include/ogc/system.h:
+C:/devkitPro/libogc/include/gcutil.h:
+C:/devkitPro/libogc/include/ogc/video.h:
+C:/devkitPro/libogc/include/ogc/video_types.h:
+C:/devkitPro/libogc/include/ogc/usbgecko.h:
+C:/devkitPro/libogc/include/ogc/video_types.h:
+C:/devkitPro/libogc/include/ogc/texconv.h:
+C:/devkitPro/libogc/include/ogc/ipc.h:
+C:/devkitPro/libogc/include/ogc/sha.h:
+C:/devkitPro/libogc/include/ogc/aes.h:
+C:/devkitPro/libogc/include/ogc/es.h:
+C:/devkitPro/libogc/include/ogc/stm.h:
+C:/devkitPro/libogc/include/ogc/ios.h:
+C:/devkitPro/libogc/include/ogc/usb.h:
+C:/devkitPro/libogc/include/ogc/isfs.h:
+C:/devkitPro/libogc/include/ogc/conf.h:
+C:/devkitPro/libogc/include/ogc/usbstorage.h:
+C:/devkitPro/libogc/include/ogc/mutex.h:
+C:/devkitPro/libogc/include/ogc/system.h:
+C:/devkitPro/libogc/include/ogc/wiilaunch.h:
+C:/devkitPro/libogc/include/asndlib.h:
+C:/devkitPro/libogc/include/wiiuse/wpad.h:
+C:/devkitPro/libogc/include/wiiuse/wiiuse.h:
+C:/devkitPro/libogc/include/bte/bte.h:
+C:/devkitPro/libogc/include/bte/bd_addr.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GUI/gui_imagedata.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/gd.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/gd_io.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/gdfx.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/gd.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/FreeTypeGX.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/ft2build.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/config/ftheader.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/freetype.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/config/ftconfig.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/config/ftoption.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/config/ftstdlib.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/fterrors.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/ftmoderr.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/fterrdef.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/fttypes.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/ftsystem.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/ftimage.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/portlibs/include/freetype/ftbitmap.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/video.h:
+C:/devkitPro/libogc/include/ogcsys.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/input.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GUI/OptionList.hpp:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/SoundOperations/gui_sound.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/SoundOperations/gui_bgm.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/SoundOperations/gui_sound.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/utils/timer.h:
+C:/devkitPro/libogc/include/ogc/lwp_watchdog.h:
+C:/devkitPro/libogc/include/ogc/lwp_queue.h:
+E:/Github/Hoshi-Iso-Builder/USBLoaderGXPlus/source/GUI/sigslot.h:
