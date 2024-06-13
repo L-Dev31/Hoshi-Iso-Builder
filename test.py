b6e079a46492d5f7768f569cf03a159e271f2cf4
Léo/L-Dev <86838693+L-Dev31@users.noreply.github.com>
2024-06-13 19:20:59 -0400

    cleanup

--- test.py
+++
@@ -1,7 +0,0 @@
-from Elements.CCPatching import hoshiRiivolutionSettingsSelection
-
-hoshiRiivolutionSettingsSelection.run_hrss(
-    "D:\\smg_modding\\FILE\\GAME\\SMG2.wbfs",
-    "D:\\smg_modding\\FILE\\MOD\\SuperMarioGravity New Demo\\riivolution\\SuperMarioGravity_NewDemo.xml",
-    "D:\\smg_modding\\FILE\\MOD\\SuperMarioGravity New Demo\\SuperMarioGravity_NewDemo"
-)
